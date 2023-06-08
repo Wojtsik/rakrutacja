f213feb07aec1ed0f372de30d00ba0b58fac9aba
Wojtsik <72934201+Wojtsik@users.noreply.github.com>
2023-06-08 12:01:58 +0200

    Add files via upload

--- formsds.js
+++ formsds.js
@@ -1,34 +1,49 @@
-const form = document.getElementById('formsds');
-const webhookUrl = 'https://discord.com/api/webhooks/1115993396306268221/DU5LSctZnTv5KQPUnv19CpbnuucRa4mq7GWtS9dScWGFEsBZbVEGGtksA28CknRz1l5a';
+// app.js
 
-form.addEventListener('submit', (event) => {
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  // Collect form data
-  const formData = new FormData(form);
+  const nick = document.querySelector('#nick').value;
+  const poziom = document.querySelector('#poziom').value;
+  const moc = document.querySelector('#moc').value;
+  const exp = document.querySelector('#exp').value;
+  const dsc = document.querySelector('#dsc').value;
+  const war = document.querySelector('#war').value;
+  const powod = document.querySelector('#message').value;
 
-  // Create payload object
-  const payload = {};
-  formData.forEach((value, key) => {
-    payload[key] = value;
-  });
+  const formData = {
+    nick: nick,
+    poziom: poziom,
+    moc: moc,
+    exp: exp,
+    dsc: dsc,
+    war: war,
+    powod: powod
+  };
 
-  // Send payload to Discord webhook
+  const webhookUrl = 'https://discord.com/api/webhooks/1115993396306268221/DU5LSctZnTv5KQPUnv19CpbnuucRa4mq7GWtS9dScWGFEsBZbVEGGtksA28CknRz1l5a'; 
+
+  // Send the form data to Discord webhook
   fetch(webhookUrl, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(payload),
+    body: JSON.stringify(formData)
   })
-    .then((response) => {
+    .then(response => {
       if (response.ok) {
-        console.log('Form data sent successfully');
+        const successMsg = document.querySelector('#success-msg');
+        successMsg.textContent = 'Wysłano!';
+        setTimeout(function() {
+          successMsg.textContent = ''; // Clear success message after 2 seconds
+        }, 2000);
       } else {
-        console.error('Error sending form data');
+        console.error('Nie udało się wysłać informacji');
       }
     })
-    .catch((error) => {
-      console.error('Error sending form data', error);
+    .catch(error => {
+      console.error('Wystąpił błąd podczas wysyłania wiadomości :', error);
     });
 });
